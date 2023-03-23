e9b1d9887fccf568c3dd883268ef6fae16c116e4
“raissonsouto” <raisson.souto@ccc.ufcg.edu.br>
2023-03-23 08:19:23 -0300

    improve algorithm and project structure

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import json
 import re
 
 data = []
-pattern = r'^(\d{8}) - (.+) - [A-Z]\s(\d+)\s\/\s(\d+)$'
+pattern = r'^(\d+) - (.+) - [A-Z] (\d+) \/ (\d+)(.*)$'
 pattern2 = r'^\d{2}\/\d{2}\/\d{4}\s\d{2}:\d{2}:\d{2}\s\d+\s\/\s\d+$'
-
 jsona = []
 
 EXCLUDE_PATTERNS = [
@@ -35,23 +34,31 @@ def get_week_day(day: int) -> str:
     return days.get(day, "")
 
 
+def camel_case(text: str) -> str:
+    words = text.split(" ")
+    capitalized_words = [word.capitalize() for word in words]
+
+    return "".join(capitalized_words)
+
+
 def filter_page(page: list) -> None:
     for line in page:
         if line in EXCLUDE_PATTERNS or \
            re.match(pattern, line) or \
-           re.match(pattern2, line):
+           re.match(pattern2, line) or\
+           line.startswith("TOTAL"):
             continue
         else:
             data.append(line)
 
 
-def to_json(course_id: str, class_name: str, professor_name: str, schedule: list, room: str) -> str:
+def to_json(course_id: str, class_name: str, professors: list, schedule: list, room: str) -> str:
     """
     Given attributes of a course, returns a JSON string representing the course.
 
     :param course_id: The ID of the course.
     :param class_name: The name of the class.
-    :param professor_name: The name of the professor.
+    :param professors: A list of the name of the professor.
     :param schedule: A list of tuples representing the schedule of the class.
     Each tuple should contain two strings representing the start and end time
     of the class.
@@ -62,7 +69,7 @@ def to_json(course_id: str, class_name: str, professor_name: str, schedule: list
     my_dict = {
         "id": course_id,
         "class": class_name,
-        "professor": professor_name,
+        "professor": professors,
         "schedule": schedule,
         "room": room
     }
@@ -70,27 +77,37 @@ def to_json(course_id: str, class_name: str, professor_name: str, schedule: list
     return json.dumps(my_dict)
 
 
-def scraping_data():  # breaks in fmcc2
+def scraping_data():
     i = 1
     while i < len(data):
-        if data[i].startswith("TOTAL"):
-            i += 1
-        print(data[i])
         id = data[i].split(" - ")[0]
-        turma = " ".join(data[i].split(" - ")[1].split()[:-6])
-        professor = data[i + 3].replace("- ", "").capitalize()
+        turma = camel_case(" ".join(data[i].split(" - ")[1].split()[:-6]))
         aulas = [
-            data[i].split(" ")[-3:-1],
-            data[i + 1].split(" ")[0:2]
+            data[i].split(" ")[-3:-1]
         ]
+        try:
+            aulas.append(data[i + 1].split(" ")[0:2])
+            aulas[1][0] = get_week_day(int(aulas[1][0]))
+        except Exception as e:
+            pass
+
         sala = data[i].split(" ")[-1]
+
+        aulas[0][0] = get_week_day(int(aulas[0][0]))
+
+
+        i += 2
+
+        professor = []
+
+        while data[i].startswith("- "):
+            professor.append(camel_case(data[i].replace("- ", "")))
+            i += 1
 
         to_json(id, turma, professor, aulas, sala)
 
-        i += 4
 
-
-with open('2022.2.pdf', 'rb') as pdf_file:
+with open('pdfs/computer-science/2022.2.pdf', 'rb') as pdf_file:
 
     pdf_reader = PyPDF2.PdfReader(pdf_file)
 
@@ -99,4 +116,8 @@ with open('2022.2.pdf', 'rb') as pdf_file:
         page_lines = pdf_page.extract_text().split('\n')
         filter_page(page_lines)
 
-    scraping_data()
+with open('temp.txt', 'w') as temp:
+    for line in data:
+        temp.write(f'{line}\n')
+
+scraping_data()
