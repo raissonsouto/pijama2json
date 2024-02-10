49644101b29a1e8b914c0721b61d5dfd18383f6f
Raisson Souto <raisson.souto@ccc.ufcg.edu.br>
2024-02-10 12:02:37 -0300

    improving metadata.json format

--- src/self_scrapper.py
+++ src/self_scrapper.py
@@ -4,11 +4,13 @@ from src.util import remove_file_extension
 
 
 def self_scrap(path: str) -> None:
-    rep_data = {}
+    rep_data = []
 
     for directory_path, _, filenames in os.walk(path):
         if directory_path.endswith(path):
             continue
+
+        course = {"name": directory_path.split("/")[-1]}
 
         semesters = []
 
@@ -16,7 +18,9 @@ def self_scrap(path: str) -> None:
             if filename.endswith('.json'):
                 semesters.append(remove_file_extension(filename))
 
-        rep_data[directory_path.split("/")[-1]] = semesters
+        course["semesters"] = semesters
+
+        rep_data.append(course)
 
     write_json(path + "/metadata.json", rep_data)
 
